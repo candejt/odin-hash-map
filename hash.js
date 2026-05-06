06d4a31bddf99b21ffc2ed3816f44963210806a5
candejt <139970803+candejt@users.noreply.github.com>
2026-05-06 13:22:24 +0200

    Add HashMap and Node classes with set, get, remove and utility methods

---
+++ hash.js
@@ -0,0 +1,146 @@
+import Node from "./node.js";
+
+class HashMap {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+    this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.CodeAt(i);
+    }
+    return hashCode % this.capacity;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    let current = this.buckets[index];
+
+    //if empty
+    if (current === null) {
+      this.buckets[index] = new Node(key, value);
+      this.size++;
+      return;
+    }
+
+    //if not empty
+    while (current !== null) {
+      if (current.key === key) {
+        current.value = value;
+        return;
+      }
+      if (current.next === null) break;
+      current = current.next;
+    }
+    current.next = new Node(key, value);
+    this.size++;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    let current = this.buckets[index];
+
+    while (current !== null) {
+      if (current.key === key) {
+        return current.value;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+
+  has(key) {
+    const index = this.hash(key);
+    let current = this.buckets[index];
+
+    while (current !== null) {
+      if (current.key === key) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    let current = this.buckets[index];  
+
+    if (current === null) return false;
+
+    if (current.key === key) {
+      this.buckets[index] = current.next;
+      this.size--;
+      return true;
+    }
+
+    let prev = null;
+    while (current !== null) {
+      if (current.key === key) {
+        prev.next = current.next;
+        this.size--;
+        return true;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return false;
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = new Array(this.buckets.capacity).fill(null);
+    this.size = 0;
+  }
+
+  keys() {
+    const result = [];
+
+    for (let i=0; i<this.buckets.length; i++){
+      let current = this.buckets[index];
+
+      while (current !== null){
+        result.push(current.key);
+        current = current.next;
+      }
+    }
+    return result;
+  }
+
+  values() {
+    const result = [];
+    
+    for (let i=0; i<this.buckets.length; i++){
+      let current = this.buckets[index];
+
+      while (current !== null){
+        result.push(current.value);
+        current = current.next;
+      }
+    }
+    return result;
+  }
+
+  entries() {
+    const result = [];
+    
+    for (let i=0; i<this.buckets.length; i++){
+      let current = this.buckets[index];
+
+      while (current !== null){
+        result.push([current.key, current.value]);
+        current = current.next;
+      }
+    }
+    return result;
+  }
+}
